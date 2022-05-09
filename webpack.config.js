372f04a0c6382f9ecfaa6cb160d76f7c77a02443
Ruslan <trybukhouskij@gmail.com>
2022-05-10 02:01:25 +0300

    feat: add dist-cleaner

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -12,8 +13,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new htmlWebpackPlugin({
+    new HTMLWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new CleanWebpackPlugin()
   ]
 }
